4f753f1ed420124d3c35ac7ea6f05a9f128e306f
Rauchdimehdi <40724965+Rauchdimehdi@users.noreply.github.com>
2019-11-21 10:55:25 +0100

    I added TextInputLayour, and Swipe option

--- app/src/main/java/com/example/user/snowtam/MainActivity.java
+++ app/src/main/java/com/example/user/snowtam/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.user.snowtam;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
+//import android.support.v4.app.GestureDetectorCompat;
 public class MainActivity extends AppCompatActivity {
+
+    private GestureDetectorCompat mGestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGestureDetector = new GestureDetectorCompat(this,new GestureListner());
+
+    }
+
+    private class GestureListner extends GestureDetector.SimpleOnGestureListener{
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            Toast.makeText(MainActivity.this,"Swipe up",Toast.LENGTH_SHORT).show();
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            Toast.makeText(MainActivity.this,"Swipe up",Toast.LENGTH_SHORT).show();
+            return super.onDoubleTap(e);
+        }
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            Toast.makeText(MainActivity.this,"Swipe up",Toast.LENGTH_SHORT).show();
+            return super.onSingleTapConfirmed(e);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mGestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 }
